ca982913bdad190f1b6470fa20bfc52e091b1125
Your Name <hoankim1610@gamil.com>
2022-09-03 11:29:42 +0700

    ss5

---
+++ THUC_HANH_MODULE_5/countdown-timer/src/app/countdown/countdown.component.ts
@@ -0,0 +1,71 @@
+import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.css']
+})
+export class CountdownComponent implements OnInit, OnChanges, OnDestroy {
+  message = '';
+  remainingTime: number;
+  @Input()
+  seconds = 11;
+  @Output()
+  finish = new EventEmitter<boolean>();
+  private intervalId = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ('seconds' in changes) {
+      let v = changes.seconds.currentValue;
+      v = typeof v === 'undefined' ? 11 : v;
+      const vFixed = Number(v);
+      this.seconds = Number.isNaN(vFixed) ? 11 : vFixed;
+    }
+  }
+
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+
+  ngOnInit() {
+    this.reset();
+  }
+
+  ngOnDestroy() {
+    this.clearTimer();
+  }
+
+  start() {
+    this.countDown();
+    if (this.remainingTime <= 0) {
+      this.remainingTime = this.seconds;
+    }
+  }
+
+  stop() {
+    this.clearTimer();
+    this.message = `Holding at T-${this.remainingTime} seconds`;
+  }
+
+  reset() {
+    this.clearTimer();
+    this.remainingTime = this.seconds;
+    this.message = `Click start button to start the Countdown`;
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.remainingTime -= 1;
+      if (this.remainingTime === 0) {
+        this.message = 'Blast off!';
+        this.clearTimer();
+        this.finish.emit(true);
+      } else {
+        this.message = `T-${this.remainingTime} seconds and counting`;
+      }
+    }, 1000);
+  }
+
+}
